4dd0ebfd73145c65b71dd64c5eb6c7f52c040ee1
Sergio <unduashe@outlook.es>
2025-03-23 12:03:47 +0100

    esquema basico de servidores y envío de mensajes sin valorar disposicion de mensajes recibidos

--- mensajeria_instantanea/src/app/Latam/page.tsx
+++ mensajeria_instantanea/src/app/Latam/page.tsx
@@ -1,8 +1,33 @@
+"use client";
+import { useRef } from "react";
+
 function Latam(){
+    const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+    
+        const adjustHeight = () => {
+            const textarea = textareaRef.current;
+            if (textarea) {
+              textarea.style.height = "auto";
+              const maxHeight = 98;
+              textarea.style.height = `${Math.min(textarea.scrollHeight, maxHeight) + 5}px`;
+            }
+          };
+
     return(
-        <h1>
-            esto es LATAM
-        </h1>
+        <div className="flex-1 w-full relative">
+            <h1>
+                esto es LATAM
+            </h1>
+            <form className="w-full flex justify-center absolute bottom-5/100">
+                <textarea 
+                ref={textareaRef}
+                className="px-3 py-1 border border-gray-200 rounded-md resize-none overflow-y-auto max-h-[50rem] w-85/100" 
+                rows={1}
+                placeholder="Mensaje" 
+                onInput={adjustHeight}/>
+                <button className="mx-5" type="submit">Enviar</button>
+            </form>
+        </div>
     )
 }
 
--- mensajeria_instantanea/src/app/components/Navbar.tsx
+++ mensajeria_instantanea/src/app/components/Navbar.tsx
@@ -6,10 +6,10 @@ function LeftNavbar() {
             <input type="text" className="border-2 border-gray-200 rounded-md p-1 w-1/1 focus:border-black" placeholder="Search chat"></input>
             <h2 className="mt-3 p-2 ps-2 font-semibold text-center">Chats</h2>
             <ul>
-                <li className="p-2 ps-2 border-b border-gray-200 hover:bg-gray-600 hover:text-white hover:rounded-md">
+                <li className="p-2 border-b border-gray-200 hover:bg-gray-600 hover:text-white hover:rounded-md">
                     <Link className="block w-full h-full" href={'/Espana'}>España</Link>
                 </li>
-                <li className="p-2 ps-2 border-b border-gray-200 hover:bg-gray-600 hover:text-white hover:rounded-md">
+                <li className="p-2 border-b border-gray-200 hover:bg-gray-600 hover:text-white hover:rounded-md">
                     <Link className="block w-full h-full" href={'/Latam'}>LATAM</Link>
                 </li>
             </ul>
