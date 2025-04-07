6cf1dc7885a40b3ebecd4b1247c0c78cd52d4745
Sergio <unduashe@outlook.es>
2025-04-07 17:51:07 +0200

    envio de mensajes funcional

--- src/app/pages/chats/[id]/page.tsx
+++ src/app/pages/chats/[id]/page.tsx
@@ -4,32 +4,34 @@ import { useParams } from 'next/navigation';
 
 import { useRef } from "react";
 
-function ChatPage(){
+function ChatPage() {
     let params = useParams();
-    let [chat, setChat] = useState([])
+    let [chat, setChat] = useState([]);
+    let [mensaje, setMensaje] = useState("");
+    let usuario = localStorage.getItem("usuario") ? localStorage.getItem("usuario") : "";
+
     useEffect(() => {
         const fetchChat = async () => {
             try {
                 const response = await fetch(`/api/chats?id=${params.id}`);
-                
+
                 if (!response.ok) {
                     const errorData = await response.json();
                     throw new Error(errorData.error || 'Error al cargar el chat');
                 }
-                
+
                 const data = await response.json();
-                console.log(data, "quiiiiiiiii");
-                
                 setChat(data);
             } catch (err) {
                 console.log(err)
-        };
-    }
-    if (params.id)
-        fetchChat();
+            };
+        }
+        if (params.id)
+            fetchChat();
     }, [params.id]);
 
-    function test(){
+    // funcion provisional
+    function test() {
         console.log(chat.fechaCreacion)
     }
 
@@ -38,34 +40,78 @@ function ChatPage(){
     const adjustHeight = () => {
         const textarea = textareaRef.current;
         if (textarea) {
-          textarea.style.height = "auto";
-          const maxHeight = 98;
-          textarea.style.height = `${Math.min(textarea.scrollHeight, maxHeight) + 5}px`;
+            textarea.style.height = "auto";
+            const maxHeight = 98;
+            textarea.style.height = `${Math.min(textarea.scrollHeight, maxHeight) + 5}px`;
         }
-      };
+    };
 
-    return(
-        <div className="flex-1 w-full relative">
+    async function envioMensaje(texto:string) {
+        try {
+            const respuesta = await fetch(`/api/mensajes?id=${params.id}&usuario=${usuario}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    contenido: mensaje
+                })
+            });
+            console.log(respuesta);
+            
+            const datos = await respuesta.json();
+            console.log(datos);
+            setMensaje("");
+            if (!usuario) localStorage.setItem("usuario", datos.msg.autor.nombreUsuario);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const manejarEnvioMensaje = async (e) => {
+        e.preventDefault();
+        await envioMensaje(mensaje)
+        console.log(mensaje);
+        
+    }
+
+    return (
+        <div className="flex-col w-full flex justify-between min-h-screen">
             <h1>
                 Bienvenido a {chat.nombre} creado el {chat.fechaCreacion}, participantes:
-                {(chat.participantes || []).length < 1 
-                ? 0
-                : <ul>
-                    {chat.participantes.map((participante) => <li key={participante.nombreUsuario}>{participante.nombreUsuario}</li>)}
-                </ul>
-            }
+                {(chat.participantes || []).length < 1
+                    ? 0
+                    : <ul>
+                        {chat.participantes.map((participante) => <li key={participante.nombreUsuario}>{participante.nombreUsuario}</li>)}
+                    </ul>
+                }
             </h1>
-                
-            <button className='w-3xl h-3' onClick={test}>aqui</button>
-            <form className="w-full flex justify-center absolute bottom-5/100">
-                <textarea 
-                ref={textareaRef}
-                className="px-3 py-1 border border-gray-200 rounded-md resize-none overflow-y-auto max-h-[50rem] w-85/100" 
-                rows={1}
-                placeholder="Mensaje" 
-                onInput={adjustHeight}/>
-                <button className="mx-5" type="submit">Enviar</button>
-            </form>
+
+            <div className='flex items-center w-full flex-col justify-between space-y-5'>
+                {(chat.mensajes || []).length < 1
+                    ? "Se el primero en enviar un mensaje!"
+                    : <ul className='w-90/100 space-y-2'>
+                        {chat.mensajes.map((mensaje) =>
+                            <li className='border-2 border-gray-200 rounded-md p-1 px-3' key={mensaje.id}>{mensaje.contenido}
+                                <span className='float-right'>{mensaje.autor.nombreUsuario}</span>
+                            </li>
+                        )}
+                    </ul>
+                }
+
+                <form className="w-90/100 flex justify-center mb-5" onSubmit={manejarEnvioMensaje}>
+                    <textarea
+                        ref={textareaRef}
+                        className="px-3 py-1 border border-gray-200 rounded-md resize-none overflow-y-auto max-h-[50rem] w-full"
+                        rows={1}
+                        placeholder="Mensaje"
+                        value={mensaje}
+                        onInput={adjustHeight}
+                        onChange={(e) => setMensaje(e.target.value)}
+                        />
+                    <button className="mx-5" type="submit">Enviar</button>
+                </form>
+            </div>
         </div>
     )
 }
