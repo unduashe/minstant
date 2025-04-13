34ebf9db6f61b75d226bed8f69760afabbd12da2
Sergio <unduashe@outlook.es>
2025-04-13 21:03:37 +0200

    creacion de archivo para guardar clases personalizadas de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+export const content = [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+];
+export const safelist = [
+    'pb-[50px]',
+    'pb-[74px]',
+    'pb-[98px]',
+    'pb-[121px]',
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
+  
