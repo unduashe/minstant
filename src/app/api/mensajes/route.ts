0f5bd93c93b4f03a7beb071c67714d95fe7d0dd0
Sergio <unduashe@outlook.es>
2025-04-14 18:19:02 +0200

    modificado endpoint mensajes para dar información de mensajes del chat

--- src/app/api/mensajes/route.ts
+++ src/app/api/mensajes/route.ts
@@ -3,23 +3,70 @@ import { NextResponse } from 'next/server';
 import { MensajeEsquema } from '../../../../lib/esquemas';
 import { moderadorContenido } from '../../../../lib/moderacion';
 import { boolean } from 'zod';
+import { GuardiaMensajeChatEspecifico } from '../../../../lib/guardiasTipo';
 
 const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
     try {
         const { searchParams } = new URL(request.url);
-        const pagina = Number(searchParams.get('pagina')) || 1;
+        const paginaParam = Number(searchParams.get('pagina')) || 1;
+        let skipParam = Number(searchParams.get('skip') || 30);
+        const chatIdParam = Number(searchParams.get('id'));
+        const usuarioParam = searchParams.get('usuario');
+
+        const chatExiste = await prisma.chats.findUnique({
+            where: {
+                id: chatIdParam
+            },
+            include: {
+                participantes: {
+                    include: {
+                        usuario: {
+                            select: {
+                                nombreUsuario: true
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        const participante = chatExiste?.participantes.some(participante => participante.usuario.nombreUsuario === usuarioParam);
+        const publico = chatExiste?.publico;
+        // validación de que hayamos obtenido el chat y de que el usuario pueda obtener información del chat
+        if (!chatExiste) return NextResponse.json({ error: "El chat solicitado no existe" }, { status: 404 });
+        if (!participante && !publico) return NextResponse.json({ error: "No dispones de acceso al chat" }, { status: 403 });
 
         const mensajes = await prisma.mensajes.findMany({
             orderBy: {
-                fechaEnvio: 'desc'
+                id: 'desc'
             },
-            skip: (pagina - 1) * 10,
-            take: 10,
+            include: {
+                autor: {
+                    select: {
+                        nombreUsuario: true
+                    }
+                }
+            },
+            skip: (paginaParam - 1) * skipParam,
+            take: 30,
         });
+        // formateo de respuesta
+        const respuestaMensajes = mensajes.map((mensaje) => {
+            const respuestaMensaje: GuardiaMensajeChatEspecifico = {
+                id: mensaje.id,
+                contenido: mensaje.contenido,
+                fechaEnvio: mensaje.fechaEnvio,
+                editado: mensaje.editado,
+                eliminado: mensaje.eliminado,
+                contenidoOriginal: mensaje.contenidoOriginal,
+                autor: mensaje.autor
+            }
+            return respuestaMensaje
+        })
 
-        return NextResponse.json(mensajes);
+        return NextResponse.json(respuestaMensajes);
 
     } catch (error) {
         return NextResponse.json(
@@ -31,7 +78,7 @@ export async function GET(request: Request) {
     }
 }
 
-export async function POST(request:Request) {
+export async function POST(request: Request) {
     try {
         // declaración de variables de datos a obtener
         const { searchParams } = new URL(request.url);
@@ -39,12 +86,12 @@ export async function POST(request:Request) {
         // validación del cuerpo de la petición
         const validacionCuerpoPeticion = MensajeEsquema.safeParse(cuerpoPeticion);
         if (!validacionCuerpoPeticion.success) {
-            return NextResponse.json({error: validacionCuerpoPeticion.error}, {status: 400});
+            return NextResponse.json({ error: validacionCuerpoPeticion.error }, { status: 400 });
         }
         // validación de obtención de id
         const idParam = Number(searchParams.get('id'));
         const usuarioParam = searchParams.get('usuario') || undefined;
-        if (!idParam || isNaN(idParam)) return NextResponse.json({error: 'Es necesario indicar el parámetro id'}, {status: 400});
+        if (!idParam || isNaN(idParam)) return NextResponse.json({ error: 'Es necesario indicar el parámetro id' }, { status: 400 });
         // validación de que la información recibida tiene el formato correcto
         interface GuardiaMensaje {
             contenido: string,
@@ -58,24 +105,24 @@ export async function POST(request:Request) {
         }
         // validación de que el chat en el que se está intentando escribir extiste
         const chat = await prisma.chats.findUnique({
-            where: {id: mensaje.chatId},
+            where: { id: mensaje.chatId },
             include: {
                 participantes: true
             }
         })
-        if (!chat) return NextResponse.json({error: 'Chat no encontrado'}, {status: 404});
+        if (!chat) return NextResponse.json({ error: 'Chat no encontrado' }, { status: 404 });
         let autorId: number;
         let usuario;
         // validación de que el usuario informado que intenta agregar información existe
-        if (mensaje.usuario && mensaje.usuario != 'null' || mensaje.usuario && mensaje.usuario != 'undefined'){
+        if (mensaje.usuario && mensaje.usuario != 'null' || mensaje.usuario && mensaje.usuario != 'undefined') {
             usuario = await prisma.usuario.findUnique({
-                where: {nombreUsuario: mensaje.usuario}
+                where: { nombreUsuario: mensaje.usuario }
             })
             if (!usuario) return NextResponse.json(
-                {error: 'El usuario con el que estás intentando enviar el mensaje no se encuentra, prueba a logarte nuevamente'},
-                {status: 404}
+                { error: 'El usuario con el que estás intentando enviar el mensaje no se encuentra, prueba a logarte nuevamente' },
+                { status: 404 }
             )
-            
+
         }
         // validación de que si no se informa usuario se crea uno, independientemente se asigna el autorId
         if (usuario) {
@@ -91,21 +138,21 @@ export async function POST(request:Request) {
         }
         // validación de que el usuario puede escribir en el chat
         const esParticipante = chat.participantes.some((participante) => participante.usuarioId === autorId);
-        if (!esParticipante && !chat.publico) return NextResponse.json({error: 'No tienes acceso para participar en el chat'}, {status: 403});
+        if (!esParticipante && !chat.publico) return NextResponse.json({ error: 'No tienes acceso para participar en el chat' }, { status: 403 });
         // agregar participante al chat público si no existía
         if (!esParticipante && chat.publico) {
-                await prisma.usuarioChats.create({
-                    data: {
-                        usuarioId: autorId,
-                        chatId: mensaje.chatId
-                    }
-                })
+            await prisma.usuarioChats.create({
+                data: {
+                    usuarioId: autorId,
+                    chatId: mensaje.chatId
+                }
+            })
         }
         // validación de que el contenido del mensaje no sea tóxico
         const resultadoModeracion = await moderadorContenido(mensaje.contenido);
         if (Object.values(resultadoModeracion).includes(true)) return NextResponse.json(
-            {error: 'El contenido del mensaje es inapropiado'}, 
-            {status: 422}
+            { error: 'El contenido del mensaje es inapropiado' },
+            { status: 422 }
         )
         // creación del mensaje
         const nuevoMensaje = await prisma.mensajes.create({
@@ -117,17 +164,17 @@ export async function POST(request:Request) {
             },
             include: {
                 autor: {
-                    select: {nombreUsuario: true}
+                    select: { nombreUsuario: true }
                 }
             }
         });
 
-        return NextResponse.json({msg: nuevoMensaje}, {status: 201})
-        
+        return NextResponse.json({ msg: nuevoMensaje }, { status: 201 })
+
     } catch (error) {
         return NextResponse.json(
-            {error: error},
-            {status: 500}
+            { error: error },
+            { status: 500 }
         )
     } finally {
         prisma.$disconnect();
